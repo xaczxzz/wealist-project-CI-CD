644e5abc4bd1736acdc3196b6c4630ef74c32447
xaczxzz <vomj12@naver.com>
2025-11-13 12:03:26 +0900

    fix

--- user-service/src/main/java/OrangeCloud/UserRepo/dto/workspace/CreateWorkspaceRequest.java
+++ user-service/src/main/java/OrangeCloud/UserRepo/dto/workspace/CreateWorkspaceRequest.java
@@ -12,5 +12,5 @@ public class CreateWorkspaceRequest {
     @NotBlank(message = "워크스페이스 이름은 필수입니다.")
     private String workspaceName;
     private String workspaceDescription;
-    private Boolean isPublic; // 검색 > 초대 가능성
+    private Boolean isPublic = false; // 검색 > 초대 가능성
 }
